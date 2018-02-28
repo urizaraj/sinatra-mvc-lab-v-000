1252689295100cf23cc20a365af213d1f6178b57
Alexander Urizar <urizaraj@gmail.com>
2018-02-28 21:08:52 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,10 +4,10 @@ class PigLatinize
   def initialize(sentence)
     self.sentence = sentence
     self.words = sentence.split(' ').map(&:downcase)
-    
+
   end
 
   def latinize_word(word)
-    
+    word
   end
 end
