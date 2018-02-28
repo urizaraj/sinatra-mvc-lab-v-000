f634954db96c545db2fbde2f9521f9f07d82ec99
Alexander Urizar <urizaraj@gmail.com>
2018-02-28 20:58:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  get '/piglatinize' do
+    @pg = PigLatinize.new(params)
+    erb :result
+  end
 end
